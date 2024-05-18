01d8213fb7ecbfde5302fcd62d89938b1cd3ef09
Bahaa <104843039+0xffvirus@users.noreply.github.com>
2024-05-18 10:58:09 +0300

    Delete clear.py

--- clear.py
+++
@@ -1,9 +0,0 @@
-def clear():
-    from sys import platform
-    from os import system
-    if platform == "linux" or platform == "linux2":
-        system("clear")
-    elif platform == "darwin":
-        system("clear")
-    elif platform == "win32":
-        system("cls")
